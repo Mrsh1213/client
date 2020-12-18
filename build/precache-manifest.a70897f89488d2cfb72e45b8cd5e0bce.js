c15ef06bc583e186aaeb0739105b215f0d093718
mrsh1213 <mohammad.shobiri@gmail.com>
2020-12-18 22:46:46 +0330

    version 0.1.1 build 8

---
+++ build/precache-manifest.a70897f89488d2cfb72e45b8cd5e0bce.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c60f52e424cc794e5d92d292190b613",
+    "url": "/index.html"
+  },
+  {
+    "revision": "87c6c238ed229d3fa6ca",
+    "url": "/static/css/2.4fcf9107.chunk.css"
+  },
+  {
+    "revision": "3e535b8476acf5447f23",
+    "url": "/static/css/main.1d907db4.chunk.css"
+  },
+  {
+    "revision": "87c6c238ed229d3fa6ca",
+    "url": "/static/js/2.6c3e74e8.chunk.js"
+  },
+  {
+    "revision": "56de15db0528e473e8447ee66bbfb67b",
+    "url": "/static/js/2.6c3e74e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e535b8476acf5447f23",
+    "url": "/static/js/main.60ea02ba.chunk.js"
+  },
+  {
+    "revision": "4744c2371fcc848ef60e",
+    "url": "/static/js/runtime-main.fe698672.js"
+  },
+  {
+    "revision": "94c311fd48c9362dea340aa3a29e3567",
+    "url": "/static/media/IRANSansWeb(FaNum).94c311fd.eot"
+  },
+  {
+    "revision": "bd6f69a8db87710b2f3fcd6ef75bd3e2",
+    "url": "/static/media/IRANSansWeb(FaNum).bd6f69a8.woff"
+  },
+  {
+    "revision": "e9908f05e5771638e40913309b784a17",
+    "url": "/static/media/IRANSansWeb(FaNum).e9908f05.ttf"
+  },
+  {
+    "revision": "0b5055ac357359f8c23320ea3dc0f78b",
+    "url": "/static/media/IRANSansWeb.0b5055ac.woff2"
+  },
+  {
+    "revision": "66b5cb30e2efe84dab915617acd39041",
+    "url": "/static/media/IRANSansWeb.66b5cb30.woff"
+  },
+  {
+    "revision": "c26a9eb202b3ef6455281f0335cfffe9",
+    "url": "/static/media/IRANSansWeb.c26a9eb2.ttf"
+  },
+  {
+    "revision": "d3931868176480f01fd4524cbd48b667",
+    "url": "/static/media/IRANSansWeb.d3931868.eot"
+  },
+  {
+    "revision": "0c40b1a0ee0e7fedeb8a4ac6c58e270b",
+    "url": "/static/media/IRANSansWeb_Medium.0c40b1a0.woff"
+  },
+  {
+    "revision": "1da39fcf35d32c87ce9b3bdcfd2b90b2",
+    "url": "/static/media/IRANSansWeb_Medium.1da39fcf.ttf"
+  },
+  {
+    "revision": "3d468556450b88fba2ef7ac695a166d3",
+    "url": "/static/media/IRANSansWeb_Medium.3d468556.eot"
+  }
+]);
